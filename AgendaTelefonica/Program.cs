8fa3f5c6415ce48b4da495d011148c97094f543b
mariosergio2025 <mariosergio.geraldo@gmail.com>
2025-12-12 07:47:29 -0300

    Add project files.

---
+++ AgendaTelefonica/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgendaTelefonica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var build = WebApplication.CreateBuilder();
+
+            build.Services.AddControllers();
+
+            var app = build.Build();
+
+            app.MapControllers();
+            app.Run();
+
+
+
+        }
+    }
+}
