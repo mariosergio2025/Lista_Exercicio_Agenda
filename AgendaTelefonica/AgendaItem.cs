0fbf57cb6d852ab476c83b12e42734105faf8f29
mariosergio2025 <mariosergio.geraldo@gmail.com>
2025-12-12 08:37:14 -0300

    Montagem Inicial antes banco dados

---
+++ AgendaTelefonica/AgendaItem.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaTelefonica
+{
+    public class AgendaItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NumTelefone { get; set; }
+    }
+}
---
+++ AgendaTelefonica/Controllers/MyControllers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaTelefonica.Controllers
+{
+    public class MyControllers : ControllerBase
+    {
+        [Route("/api/agenda")]
+        [HttpGet]
+        public ActionResult VerTodos()
+        {
+            return Ok("Deu Certo");
+        }
+        [HttpGet("{id}")]
+        public ActionResult VerItem(int id)
+        {
+            return Ok("Deu Certo");
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public ActionResult DeletaItem(int id)
+        {
+            return Ok("Deu Certo");
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult AtualizaItem(int id)
+        {
+            return Ok("Deu Certo");
+        }
+
+        [HttpPost]
+        public ActionResult CriaItem(int id)
+        {
+
+            return Created();
+        }
+
+    }
+}
